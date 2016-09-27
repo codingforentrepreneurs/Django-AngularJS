9aabf2a2a2ac67f9e401d0b2a5c948384f01a216
CFE <hello@teamcfe.com>
2016-09-27 16:20:37 -0700

    6 - Single Page App Template View

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -18,28 +18,26 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-
-
+from django.views.generic.base import TemplateView
 from rest_framework_jwt.views import obtain_jwt_token
 
-from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
-    
     url(r'^admin/', admin.site.urls),
-    url(r'^comments/', include("comments.urls", namespace='comments')),
-    
-    url(r'^register/', register_view, name='register'),
-    url(r'^login/', login_view, name='login'),
-    url(r'^logout/', logout_view, name='logout'),
-    url(r'^', include("posts.urls", namespace='posts')),
     url(r'^api/auth/token/', obtain_jwt_token),
     url(r'^api/users/', include("accounts.api.urls", namespace='users-api')),
     url(r'^api/comments/', include("comments.api.urls", namespace='comments-api')),
     url(r'^api/posts/', include("posts.api.urls", namespace='posts-api')),
-    #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns +=  [
+    url(r'', TemplateView.as_view(template_name='ang/home.html'))
+]
+
+
+
+
