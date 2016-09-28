06d2c1a7302b181b406089f9c853a72375f7bcdb
CFE <hello@teamcfe.com>
2016-09-27 17:24:57 -0700

    11 - Backend API Serializer Updates

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -71,6 +71,7 @@ class PostDetailSerializer(ModelSerializer):
 class PostListSerializer(ModelSerializer):
     url = post_detail_url
     user = UserDetailSerializer(read_only=True)
+    image = SerializerMethodField()
     class Meta:
         model = Post
         fields = [
@@ -79,8 +80,16 @@ class PostListSerializer(ModelSerializer):
             'title',
             'content',
             'publish',
+            'slug',
+            'image',
         ]
 
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image = None
+        return image
 
 
 
