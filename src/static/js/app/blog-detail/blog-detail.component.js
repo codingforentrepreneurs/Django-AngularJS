ed3326c8dac9647592f6256b66a51ed8dc045300
CFE <hello@teamcfe.com>
2016-09-28 13:09:55 -0700

    14 - Create Comment in Backend with

--- src/static/js/app/blog-detail/blog-detail.component.js
+++ src/static/js/app/blog-detail/blog-detail.component.js
@@ -3,9 +3,12 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/api/templates/blog-detail.html',
-        controller: function(Post, $http, $location, $routeParams, $scope){
-            Post.get({"slug": $routeParams.slug}, function(data){
+        controller: function(Post, $cookies, $http, $location, $routeParams, $scope){
+            
+            var slug = $routeParams.slug
+            Post.get({"slug": slug}, function(data){
                 $scope.post = data
+                $scope.comments = data.comments
             })
             
             // Post.query(function(data){
@@ -34,18 +37,47 @@ angular.module('blogDetail').
                 // someResource.$delete()
             }
 
+// "Authorization: JWT " '{"content":"some reply to another try"}' ''
+
 
             $scope.addReply = function() {
                 console.log($scope.reply)
-                $scope.comments.push($scope.reply)
-                // $scope.post.comments.push("abc")
-                resetReply()
+                var token = $cookies.get("token")
+                if (token){
+                    var req = {
+                        method: "POST",
+                        url: 'http://127.0.0.1:8000/api/comments/create/?slug=' + slug + '&type=post',
+                        data: {
+                            content: $scope.reply.content
+                        },
+                        headers: {
+                            authorization: "JWT " + token
+                        }
+                    }
+
+                    var requestAction = $http(req)
+
+                    requestAction.success(function(r_data, r_status, r_headers, r_config){
+                         $scope.comments.push($scope.reply)
+                         resetReply()
+                    })
+                    requestAction.error(function(e_data,e_status, e_headers, e_config){
+                        console.log(e_data)
+                    })
+
+                    // $scope.post.comments.push("abc")
+                    
+
+                } else {
+                    console.log("no token")
+                }
+                
             }
 
             function resetReply(){
               $scope.reply = {
-                          "id": $scope.comments.length + 1,
-                          "text": "",
+                          id: $scope.comments.length + 1,
+                          content: "",
               }
             }
 
